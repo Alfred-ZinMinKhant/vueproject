0bcedb929579e8c4650840d247c1a1fb0fcf80c4
ZinMinKhant-Alfred <zinminkhant.alfred@gmail.com>
2024-11-16 21:13:48 +0700

    Vueproject

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import Homecard from "@/components/HomeCard.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+<template>
+  <Hero />
+  <Homecard />
+  <JobListings :limit="3" :showbutton="true" />
+</template>
